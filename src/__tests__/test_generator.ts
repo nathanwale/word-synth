b8de6fe137ea19145e6dc5f7bf80bb6cdefa532a
nathanwale <48480798+nathanwale@users.noreply.github.com>
2022-08-08 20:02:32 +1000

    +generate_many, String => string

--- src/__tests__/test_generator.ts
+++ src/__tests__/test_generator.ts
@@ -1,5 +1,5 @@
 import { expect, test } from '@jest/globals'
-import { generate, Generator } from '../models/Generator'
+import { generate, generate_many, Generator } from '../models/Generator'
 
 import { Template } from '../models/Template'
 import { Voicing } from '../models/Voicing'
@@ -34,5 +34,13 @@ test("Should generate one of: [ai, aci, acci]", () => {
         templates: [temp_init, temp_mid, temp_final]
     }
     expect(['ai', 'aci', 'acci']).toContain(generate(gen))
+})
+
+test("generate_many(gen, 10) should produce an array of length 10", () => {
+    const gen: Generator = {
+        templates: [temp_init, temp_mid, temp_final]
+    }
+    const words = generate_many(gen, 10)
+    expect(words).toHaveLength(10)
 })
 
--- src/models/Generator.ts
+++ src/models/Generator.ts
@@ -2,7 +2,7 @@ import * as Template from './Template'
 /*
 ** Represents a word generated from the system
 */
-export type Word = String
+export type Word = string
 
 /*
 ** Represents the initial parameters to generate a word
@@ -15,4 +15,13 @@ export function generate(generator: Generator): Word
 {
     return generator.templates.map(
         (t) => Template.fill(t)).join("")
+}
+
+export function generate_many(generator: Generator, n: number): Word[]
+{
+    let words = []
+    for (let i = 0; i < n; i++) {
+        words.push(generate(generator))
+    }
+    return words
 }
