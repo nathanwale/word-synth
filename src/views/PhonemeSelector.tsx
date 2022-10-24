ceb9a193d953ce86c2b13edf9e4eb118781f30a7
nathanwale <48480798+nathanwale@users.noreply.github.com>
2022-10-24 13:19:29 +1100

    Use PhonemeSelection[], selected_phonemes_updater
    phoneme_selections: PhonemeSelection[] instead of separate lists of available and selected.
    
    selected_phonemes_updater: called when phoneme selections are changed

--- src/views/PhonemeSelector.tsx
+++ src/views/PhonemeSelector.tsx
@@ -2,22 +2,48 @@ import React from 'react';
 import { Phoneme } from '../models/Phoneme';
 import { PhonemeToggleButton } from './PhonemeToggleButton';
 
+export type PhonemeSelection = {
+    phoneme: Phoneme, 
+    selected: boolean,
+}
+
 export type PhonemeSelectorProps = {
     title: string,
     count: number,
-    available_phonemes: Phoneme[],
-    selected_phonemes: Phoneme[],
-    display: boolean
+    phoneme_selections: PhonemeSelection[],
+    display: boolean,
+    selected_phonemes_updater: (phoneme_selections: PhonemeSelection[]) => void
 }
 
 export function PhonemeSelector(props: PhonemeSelectorProps): JSX.Element
 {
+    let [phoneme_selections, set_phoneme_selections] = React.useState(props.phoneme_selections)
+
+    function update_selected_phoneme(phoneme: Phoneme, selected: boolean) {
+        let new_list = phoneme_selections.map(phs => {
+            if (phs.phoneme === phoneme) {
+                return {
+                    phoneme: phoneme,
+                    selected: selected,
+                }
+            } 
+            return phs
+        })
+        set_phoneme_selections(new_list)
+    }
+
+    React.useEffect(() => {
+        props.selected_phonemes_updater(phoneme_selections)
+    }, [phoneme_selections])
+
     if (props.display) {
-        let phoneme_toggles = props.available_phonemes.map(
-            (ph) => 
+        let phoneme_toggles = phoneme_selections.map(
+            (ph_selection) => 
                 <PhonemeToggleButton  
-                    key={ ph } phoneme={ ph }
-                    selected={ props.selected_phonemes.includes(ph) } />
+                    key={ ph_selection.phoneme } 
+                    phoneme={ ph_selection.phoneme }
+                    selected={ ph_selection.selected }
+                    selection_updater={ update_selected_phoneme } />
         )
         return (
             <div className='phoneme-selector'>
