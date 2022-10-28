cab7bef3554f4dbf38fe50b8b912d3f94b3dbf88
nathanwale <48480798+nathanwale@users.noreply.github.com>
2022-10-28 13:03:17 +1100

    Stub for handling updates from PhonemePool

--- src/views/WordSection.tsx
+++ src/views/WordSection.tsx
@@ -10,15 +10,26 @@ export type WordSectionProps = {
 
 export function WordSection(props: WordSectionProps)
 {
+    // selected_phonemes_updater: (phoneme_selections: PhonemeSelection[]) => void
+    function update_vowels(new_phoneme_selections: PhonemeSelection[]) {
+
+    }
+
+    function update_consonants(new_phoneme_selections: PhonemeSelection[]) {
+
+    }
+
     return (
         <div className='word-section'>
             <header>{ props.title }</header>
             <PhonemePool 
                 title='Vowels' 
-                phoneme_selections={ props.vowels } />
+                phoneme_selections={ props.vowels } 
+                selected_phonemes_updater={ update_vowels } />
             <PhonemePool 
                 title='Consonants' 
-                phoneme_selections={ props.consonants } />
+                phoneme_selections={ props.consonants } 
+                selected_phonemes_updater={ update_consonants } />
         </div>
     )
 }
