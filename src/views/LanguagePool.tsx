cea7c87730d9e87df23719a9f2a6c4c0e1192d59
nathanwale <48480798+nathanwale@users.noreply.github.com>
2022-11-01 22:51:38 +1100

    Attempt to use a reducer style

--- src/views/LanguagePool.tsx
+++ src/views/LanguagePool.tsx
@@ -1,69 +1,88 @@
 import React from 'react';
 import { PhonemePool } from './PhonemePool';
-import { PhonemeSelection, selected_phonemes } from '../models/PhonemeSelection'
-import { Phoneme } from '../models/Phoneme'
+import { PhonemeSelection } from '../models/PhonemeSelection'
+
+export type State = {
+    vowels: {
+        simple: PhonemeSelection[],
+        complex: PhonemeSelection[],
+    },
+    consonants: {
+        simple: PhonemeSelection[],
+        complex: PhonemeSelection[],
+    },
+}
+
+export enum Msg {
+    SelectSimpleVowels = "select simple vowels",
+    SelectComplexVowels = "select complex vowels",
+    SelectSimpleConsonants = "select simple consonants",
+    SelectComplexConsonants = "select complex consonants",
+}
+
+type Action =
+    | [Msg.SelectSimpleVowels, PhonemeSelection[]]
+    | [Msg.SelectComplexVowels, PhonemeSelection[]]
+    | [Msg.SelectSimpleConsonants, PhonemeSelection[]]
+    | [Msg.SelectComplexConsonants, PhonemeSelection[]]
+
+export function reducer(state: State, action: Action): State
+{
+    let [msg, payload] = action
+    console.log(`<${msg}>:`, payload)
+    state = {...state}
+    switch (msg) {
+        case Msg.SelectSimpleVowels:
+            state.vowels.simple = payload as PhonemeSelection[]
+            break
+        case Msg.SelectComplexVowels:
+            state.vowels.complex = payload as PhonemeSelection[]
+            break
+        case Msg.SelectSimpleConsonants:
+            state.consonants.simple = payload as PhonemeSelection[]
+            break
+        case Msg.SelectComplexConsonants:
+            state.consonants.complex = payload as PhonemeSelection[]
+            break
+    }
+    return state
+}
 
 export type LanguagePoolProps = {
-    simple_vowels: PhonemeSelection[],
-    complex_vowels: PhonemeSelection[], 
-    simple_consonants: PhonemeSelection[],
-    complex_consonants: PhonemeSelection[],
-    vowels_updater: (vowels: Phoneme[]) => void,
-    consonants_updater: (vowels: Phoneme[]) => void,
+    state: State,
+    updated: (s: State) => void
 }
 
 
 
 export function LanguagePool(props: LanguagePoolProps)
 {
-    function concat_phoneme_selections(phs1: PhonemeSelection[], phs2: PhonemeSelection[]): Phoneme[] {
-        return selected_phonemes(phs1)
-            .concat(selected_phonemes(phs2))
-    }
+    let [state, dispatch] = React.useReducer(reducer, props.state)
 
-    function update_simple_vowels(new_phoneme_selections: PhonemeSelection[]) {
-        let new_phons = selected_phonemes(new_phoneme_selections)
-        props.vowels_updater(new_phons)
-        console.log(`<LanguagePool> new simple vowels: ${new_phons}`)
-    }
-
-    function update_complex_vowels(new_phoneme_selections: PhonemeSelection[]) {
-        let new_phons = selected_phonemes(new_phoneme_selections)
-        props.vowels_updater(new_phons)
-        console.log(`<LanguagePool> new complex vowels: ${new_phons}`)
-    }
-
-    function update_simple_consonants(new_phoneme_selections: PhonemeSelection[]) {
-        let new_phons = selected_phonemes(new_phoneme_selections)
-        props.vowels_updater(new_phons)
-        console.log(`<LanguagePool> new simple cons: ${new_phons}`)
-    }   
-
-    function update_complex_consonants(new_phoneme_selections: PhonemeSelection[]) {
-        let new_phons = selected_phonemes(new_phoneme_selections)
-        props.vowels_updater(new_phons)
-        console.log(`<LanguagePool> new complex cons: ${new_phons}`)
-    }
+    React.useEffect(() => {
+        console.log("Raising state from <LanguagePool>", state)
+        props.updated(state)
+    }, [state])
 
     return (
         <div className='language-pool'>
             <header>Language<br />Phonemes</header>
             <PhonemePool 
                 title='Simple vowels' 
-                phoneme_selections={ props.simple_vowels } 
-                selected_phonemes_updater={ update_simple_vowels } />
+                phoneme_selections={ state.vowels.simple } 
+                selected_phonemes_updater={ phs => dispatch([Msg.SelectSimpleVowels, phs]) } />
             <PhonemePool 
                 title='Complex vowels' 
-                phoneme_selections={ props.complex_vowels }
-                selected_phonemes_updater={ update_complex_vowels }  />
+                phoneme_selections={ state.vowels.complex }
+                selected_phonemes_updater={ phs => dispatch([Msg.SelectComplexVowels, phs]) }  />
             <PhonemePool 
                 title='Simple consonants' 
-                phoneme_selections={ props.simple_consonants }
-                selected_phonemes_updater={ update_simple_consonants }  />
+                phoneme_selections={ state.consonants.simple }
+                selected_phonemes_updater={ phs => dispatch([Msg.SelectSimpleConsonants, phs]) }  />
             <PhonemePool 
                 title='Complex consonants' 
-                phoneme_selections={ props.complex_consonants }
-                selected_phonemes_updater={ update_complex_consonants }  />
+                phoneme_selections={ state.consonants.complex }
+                selected_phonemes_updater={ phs => dispatch([Msg.SelectComplexConsonants, phs]) }  />
         </div>
     )
 }
--- src/views/WordSection.tsx
+++ src/views/WordSection.tsx
@@ -2,34 +2,61 @@ import React from 'react';
 import { PhonemePool } from './PhonemePool';
 import { PhonemeSelection } from '../models/PhonemeSelection'
 
-export type WordSectionProps = {
-    title: string,
+export type State = {
     vowels: PhonemeSelection[],
     consonants: PhonemeSelection[],
 }
 
+export enum Msg {
+    SelectVowels = "select vowels",
+    SelectConsonants = "select consonants",
+}
+
+type Action =
+    | [Msg.SelectVowels, PhonemeSelection[]]
+    | [Msg.SelectConsonants, PhonemeSelection[]]
+
+export function reducer(state: State, action: Action): State
+{
+    let [msg, payload] = action
+    console.log(`<${msg}>:`, payload)
+    state = {...state}
+    switch (msg) {
+        case Msg.SelectVowels:
+            state.vowels = payload as PhonemeSelection[]
+            break
+        case Msg.SelectConsonants:
+            state.consonants = payload as PhonemeSelection[]
+            break
+    }
+    return state
+}
+
+export type WordSectionProps = {
+    title: string,
+    state: State,
+    updated: (s: State) => void,
+}
+
 export function WordSection(props: WordSectionProps)
 {
-    // selected_phonemes_updater: (phoneme_selections: PhonemeSelection[]) => void
-    function update_vowels(new_phoneme_selections: PhonemeSelection[]) {
+    let [state, dispatch] = React.useReducer(reducer, {...props.state})
 
-    }
-
-    function update_consonants(new_phoneme_selections: PhonemeSelection[]) {
-
-    }
+    React.useEffect(() => {
+        console.log("Raising state from <WordSection>", props.state)
+    }, [state])
 
     return (
         <div className='word-section'>
             <header>{ props.title }</header>
             <PhonemePool 
                 title='Vowels' 
-                phoneme_selections={ props.vowels } 
-                selected_phonemes_updater={ update_vowels } />
+                phoneme_selections={ props.state.vowels } 
+                selected_phonemes_updater={ state => dispatch([Msg.SelectVowels, state]) } />
             <PhonemePool 
                 title='Consonants' 
-                phoneme_selections={ props.consonants } 
-                selected_phonemes_updater={ update_consonants } />
+                phoneme_selections={ props.state.consonants } 
+                selected_phonemes_updater={ state => dispatch([Msg.SelectConsonants, state]) } />
         </div>
     )
 }
