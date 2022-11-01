27f1a1975d8481f1029671df74b582ce99c90f39
nathanwale <48480798+nathanwale@users.noreply.github.com>
2022-11-01 22:55:16 +1100

    First attempt to use Reducer style

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -7,10 +7,65 @@ import * as Random from '../random'
 import React from 'react';
 import { GeneratedWords } from './GeneratedWords';
 import './style/App.scss';
-import { WordSection } from './WordSection'
-import { LanguagePool } from './LanguagePool'
+import { LanguagePool, State as LanguagePoolState } from './LanguagePool'
 import { Phoneme } from '../models/Phoneme'
-import { PhonemeSelection, create_phoneme_selections } from '../models/PhonemeSelection'
+import { create_phoneme_selections } from '../models/PhonemeSelection'
+import { WordSections, State as WordSectionsState } from './WordSections'
+
+export type State = {
+    language_pool: LanguagePoolState,
+    word_sections: WordSectionsState,
+    generated_words: string[],
+}
+
+export enum Msg {
+    UpdateLanguagePool = "update language pool",
+    UpdateWordSections = "update word sections",
+    GenerateWords = "generate words",
+}
+
+type Action =
+    | [Msg.UpdateLanguagePool, LanguagePoolState]
+    | [Msg.UpdateWordSections, WordSectionsState]
+    | [Msg.GenerateWords, []]
+
+export function reducer(state: State, action: Action): State
+{
+    let [msg, payload] = action
+    console.log(`<${msg}>:`, payload)
+    switch (msg) {
+        case Msg.UpdateLanguagePool:
+            payload = payload as LanguagePoolState
+            return {
+                ...state,
+                word_sections: {
+                    initial: {
+                        vowels: [...payload.vowels.simple, ...payload.vowels.complex],
+                        consonants: [...payload.consonants.simple, ...payload.consonants.complex],
+                    },
+                    middle: {
+                        vowels: [...payload.vowels.simple, ...payload.vowels.complex],
+                        consonants: [...payload.consonants.simple, ...payload.consonants.complex],
+                    },
+                    final: {
+                        vowels: [...payload.vowels.simple, ...payload.vowels.complex],
+                        consonants: [...payload.consonants.simple, ...payload.consonants.complex],
+                    },
+                }
+            }
+        case Msg.UpdateWordSections:
+            return {
+                ...state,
+                word_sections: payload as WordSectionsState
+            }
+        case Msg.GenerateWords:
+            return {
+                ...state,
+                generated_words: payload as string[]
+            }
+    }
+    throw new Error(`Message <${msg}> not handled`)
+}
 
 
 const simple_vowels = Random.take(Vowels.simple, 5)
@@ -42,8 +97,36 @@ const temp_final: Template = {
     consonants: Random.take(Consonants.final, 15)
 }
 
+function init_state(): State {
+    let s = {
+        language_pool: {
+            vowels: {
+                simple: create_phoneme_selections(Vowels.simple, simple_vowels),
+                complex: create_phoneme_selections(Vowels.complex, complex_vowels),
+            },
+            consonants: {
+                simple: create_phoneme_selections(Consonants.simple, simple_consonants),
+                complex: create_phoneme_selections(Consonants.complex, complex_consonants),
+            }
+        },
+        word_sections: {
+            initial: {vowels: [], consonants: []},
+            middle: {vowels: [], consonants: []},
+            final: {vowels: [], consonants: []},
+        },
+        generated_words: []
+    }
+
+    console.log("init state:", s)
+    return s
+}
+
+
 function App() 
 {
+
+    let [state, dispatch] = React.useReducer(reducer, init_state())
+    let Context = React.createContext(null)
     let [lang_pool_vowels, set_lang_pool_vowels] = React.useState(lang_vowels)
     let [lang_pool_cons, set_lang_pool_cons] = React.useState(lang_consonants)
     let [initial_vowels, set_initial_vowels] = React.useState(create_phoneme_selections(lang_pool_vowels, lang_pool_vowels))
@@ -66,6 +149,10 @@ function App()
         set_initial_vowels(() => create_phoneme_selections(lang_pool_vowels, lang_pool_vowels))
         console.log(`<App> new initial_vowels: ${lang_pool_vowels}`)
     }, [lang_pool_vowels])
+
+    React.useEffect(() => {
+        console.log(`<App> state updated`, state)
+    }, [state])
 
     return (
         <div className="App">
