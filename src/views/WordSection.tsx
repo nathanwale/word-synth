0c0d6d15b39807b72160ffbfa444edd8f97f26ca
nathanwale <48480798+nathanwale@users.noreply.github.com>
2022-11-04 14:52:03 +1100

    Removed redundant code

--- src/views/WordSection.tsx
+++ src/views/WordSection.tsx
@@ -2,35 +2,19 @@ import React from 'react';
 import { PhonemePool } from './PhonemePool';
 import { PhonemeSelection } from '../models/PhonemeSelection'
 
+/*
+** State
+*/
+
 export type State = {
     vowels: PhonemeSelection[],
     consonants: PhonemeSelection[],
 }
 
-// export enum Msg {
-//     SelectVowels = "select vowels",
-//     SelectConsonants = "select consonants",
-// }
 
-// type Action =
-//     | [Msg.SelectVowels, PhonemeSelection[]]
-//     | [Msg.SelectConsonants, PhonemeSelection[]]
-
-// export function reducer(state: State, action: Action): State
-// {
-//     let [msg, payload] = action
-//     console.log(`<${msg}>:`, payload)
-//     state = {...state}
-//     switch (msg) {
-//         case Msg.SelectVowels:
-//             state.vowels = payload as PhonemeSelection[]
-//             break
-//         case Msg.SelectConsonants:
-//             state.consonants = payload as PhonemeSelection[]
-//             break
-//     }
-//     return state
-// }
+/*
+** Props
+*/
 
 export type WordSectionProps = {
     title: string,
@@ -39,24 +23,13 @@ export type WordSectionProps = {
     updated_consonants: (phs: PhonemeSelection[]) => void
 }
 
+
+/*
+** View
+*/
+
 export function WordSection(props: WordSectionProps)
 {
-    // let [state, dispatch] = React.useReducer(reducer, {...props.state})
-
-
-    // function updater(phoneme_selections: PhonemeSelection[], phoneme: Phoneme, selected: boolean, msg: Msg) {
-    //     let new_list = phoneme_selections.map(phs => {
-    //         if (phs.phoneme === phoneme) {
-    //             return {
-    //                 phoneme: phoneme,
-    //                 selected: selected,
-    //             }
-    //         } 
-    //         return phs
-    //     })
-    //     dispatch([msg, new_list])
-    // }
-
     return (
         <div className='word-section'>
             <header>{ props.title }</header>
--- src/views/WordSectionGroup.tsx
+++ src/views/WordSectionGroup.tsx
@@ -2,61 +2,26 @@ import { WordSection, State as WordSectionState } from './WordSection';
 import * as context from './context'
 import { Msg } from './reducer'
 
+
+/*
+** State
+*/
+
 export type State = {
     initial: WordSectionState,
     middle: WordSectionState,
     final: WordSectionState,
 }
 
-// export enum Msg {
-//     UpdatedInitial = "updated initial word section",
-//     UpdatedMiddle = "updated middle word section",
-//     UpdatedFinal = "updated final word section",
-// }
 
-// type Action =
-//     | [Msg.UpdatedInitial, WordSectionState]
-//     | [Msg.UpdatedMiddle, WordSectionState]
-//     | [Msg.UpdatedFinal, WordSectionState]
-
-// export function reducer(state: State, action: Action): State
-// {
-//     let [msg, payload] = action
-//     console.log(`<${msg}>:`, payload)
-//     state = {...state}
-//     switch (msg) {
-//         case Msg.UpdatedInitial:
-//             state.initial = payload as WordSectionState
-//             break
-//         case Msg.UpdatedMiddle:
-//             state.middle = payload as WordSectionState
-//             break
-//         case Msg.UpdatedFinal:
-//             state.final = payload as WordSectionState
-//             break
-//     }
-//     return state
-// }
-
-export type WordSectionGroupProps = {
-    state: State,
-}
-
-
+/*
+** View
+*/
 
 export function WordSectionGroup()
 {
-    // let [state, dispatch] = React.useReducer(reducer, {...props.state})
     let state = context.useStateContext().word_sections
     let dispatch = context.useDispatchContext()
-
-    // React.useEffect(() => {
-    //     console.log("Raising state from <WordSections>")
-    // }, [state])
-
-    // React.useEffect(() => {
-
-    // })
 
     return (
         <div className='phonemes'>
