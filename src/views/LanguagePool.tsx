b723145a75b992be13de56b3dfc3e0b2347167c6
nathanwale <48480798+nathanwale@users.noreply.github.com>
2022-11-04 14:34:16 +1100

    Removed redundant code

--- src/views/LanguagePool.tsx
+++ src/views/LanguagePool.tsx
@@ -3,6 +3,10 @@ import { PhonemeSelection } from '../models/PhonemeSelection'
 import * as context from './context'
 import { Msg } from './reducer'
 
+/*
+** State
+*/
+
 export type State = {
     vowels: {
         simple: PhonemeSelection[],
@@ -14,70 +18,15 @@ export type State = {
     },
 }
 
-// export enum Msg {
-//     SelectSimpleVowels = "select simple vowels",
-//     SelectComplexVowels = "select complex vowels",
-//     SelectSimpleConsonants = "select simple consonants",
-//     SelectComplexConsonants = "select complex consonants",
-// }
 
-// type Action =
-//     | [Msg.SelectSimpleVowels, PhonemeSelection[]]
-//     | [Msg.SelectComplexVowels, PhonemeSelection[]]
-//     | [Msg.SelectSimpleConsonants, PhonemeSelection[]]
-//     | [Msg.SelectComplexConsonants, PhonemeSelection[]]
-
-// export function reducer(state: State, action: Action): State
-// {
-//     let [msg, payload] = action
-//     console.log(`<${msg}>:`, payload)
-//     state = {...state}
-//     switch (msg) {
-//         case Msg.SelectSimpleVowels:
-//             state.vowels.simple = payload as PhonemeSelection[]
-//             break
-//         case Msg.SelectComplexVowels:
-//             state.vowels.complex = payload as PhonemeSelection[]
-//             break
-//         case Msg.SelectSimpleConsonants:
-//             state.consonants.simple = payload as PhonemeSelection[]
-//             break
-//         case Msg.SelectComplexConsonants:
-//             state.consonants.complex = payload as PhonemeSelection[]
-//             break
-//     }
-//     return state
-// }
-
-export type LanguagePoolProps = {
-    state: State,
-}
-
-
+/*
+** View
+*/
 
 export function LanguagePool()
 {
-    // let [state, dispatch] = React.useReducer(reducer, props.state)
     let state = context.useStateContext().language_pool
     let dispatch = context.useDispatchContext()
-
-    // React.useEffect(() => {
-    //     console.log("Raising state from <LanguagePool>", state)
-    //     props.updated(state)
-    // }, [state])
-
-    // function updater(phoneme_selections: PhonemeSelection[], phoneme: Phoneme, selected: boolean, msg: context.Msg) {
-    //     let new_list = phoneme_selections.map(phs => {
-    //         if (phs.phoneme === phoneme) {
-    //             return {
-    //                 phoneme: phoneme,
-    //                 selected: selected,
-    //             }
-    //         } 
-    //         return phs
-    //     })
-    //     dispatch([msg, new_list])
-    // }
 
     return (
         <div className='language-pool'>
